1788641a1122af676ec761b99a066456b55b49eb
Zq <Zq@123.com>
2022-07-03 10:08:59 +0800

    gitee commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("pull test!");
         System.out.println("gitee test!");
         System.out.println("gitee test2!");
+        System.out.println("gitee test2!");
     }
 }
