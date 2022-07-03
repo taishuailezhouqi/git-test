1db33bd4ea60fcf4063a45a33102aad75ef9eaf7
tslzq <taishuailezhouqi@163.com>
2022-07-03 02:11:25 +0000

    update src/main/java/com/atguigu/git/GitTest.java.

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("gitee test!");
         System.out.println("gitee test2!");
         System.out.println("gitee test2!");
+        System.out.println("gitee test3!");
     }
 }
